2d7ca64f0c98ea006d36a3f2d955a4e19b71ff34
Ina Costin <ynacostin@gmail.com>
2020-02-15 17:50:58 +0200

    swebsite setup, scss multiple components added

---
+++ src/js/login.js
@@ -0,0 +1,29 @@
+const serverUrl = "http://localhost:3000";
+var loginToken = localStorage.getItem("loginToken");
+if (loginToken) {
+  window.location.href = "/";
+}
+function login(e) {
+  e.preventDefault();
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var loginDetails = {
+    email: email,
+    password: password
+  };
+
+  fetch(`${serverUrl}/api/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(loginDetails)
+  })
+    .then(response => response.json())
+    .then(result => {
+      localStorage.setItem("loginToken", result.token);
+      window.location.href = "/";
+    });
+}
+
+document.getElementById("login-form").addEventListener("submit", login);
